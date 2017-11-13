564a9b75c37f329799671e842af9c577cc05fe79
mariachertkova <33563921+mariachertkova@users.noreply.github.com>
2017-11-13 20:36:34 +0800

    Update poezda.cpp

--- poezda.cpp
+++ poezda.cpp
@@ -1,18 +1,17 @@
 #include <iostream> 
 using namespace std;
 int main() { 
-	setlocale(LC_ALL, "Russian");
 	int h1, m1, h2, m2; 
-	cout<<"¬ведите врем€ отправлени€ (hh mm): "; 
+	cout<<"Vvedite vremya otpravleniya (hh mm): "; 
 	cin>>h1>>m1; 
-	cout<<"¬ведите врем€ в пути (hh mm): "; 
+	cout<<"Vvedite vremya v puti(hh mm): "; 
 	cin>>h2>>m2; 
 	int d3, h3, m3; 
 	m3=(m1+m2)%60; 
 	int hext=(m1+m2)/60; 
 	h3=(h1+h2+hext)%24; 
 	d3=1+(h1+h2+hext)/24; 
-	cout<<"ƒень и врем€ прибыти€ поезда"<<d3<<", "<<h3<<":"<<m3<<endl;
+	cout<<"Dni i vremya pribitiya poezda"<<d3<<", "<<h3<<":"<<m3<<endl;
 	system ("pause");
 	return 0; 
 } 
